52811a0dc4a58ee02e54a1ef29d1a2a85e84dfa7
PaulaSanchez <46799459+PaulaSanchez810@users.noreply.github.com>
2020-10-02 20:11:28 -0500

    terminado hasta punto 5

--- MyBatis_Introduction_VideoRental/src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/mappers/ClienteMapper.java
+++ MyBatis_Introduction_VideoRental/src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/mappers/ClienteMapper.java
@@ -31,6 +31,8 @@ public interface ClienteMapper {
      * Consultar todos los clientes
      * @return 
      */
-    public List<Cliente> consultarClientes();
+    public List<Cliente> consultarClientes(
+
+    );
     
 }
